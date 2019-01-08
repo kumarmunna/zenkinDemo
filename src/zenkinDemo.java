541aff6692aafbe966b276d1ae55f17f972d1868
Santosh Singh <educareer90@gmail.com>
2019-01-08 17:30:59 +0530

    added console logs

--- src/zenkinDemo.java
+++ src/zenkinDemo.java
@@ -4,7 +4,7 @@ public class zenkinDemo {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(" zenkins demo Valuse: "+ i);
-			
+			NullPointerException
 		}
 	}
 }
