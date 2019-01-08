1893aaef6ea5a4734b6011c6e58c444a81295f8f
Santosh Singh <educareer90@gmail.com>
2019-01-08 18:19:48 +0530

    added console logs

--- src/zenkinDemo.java
+++ src/zenkinDemo.java
@@ -3,7 +3,8 @@ public class zenkinDemo {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println(" zenkins demo Valuse: "+ i)
+			System.out.println(" zenkins demo Valuse: "+ i);
+			System.out.println(" zenkins demo Valuse: ");
 		}
 	}
 }
